ccaeb2ffb9440b9d9ed2631b015c3279fede46eb
13NOONE37 <oliwerklauze22@gmail.com>
2024-07-12 11:59:09 +0200

    basic structure: paths, assets and directories

---
+++ src/icons/icon-arrow-down.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+		const SVG = () => {
+		  return (
+			<svg width="10" height="7" xmlns="http://www.w3.org/2000/svg"><path d="M1 1l4 4 4-4" stroke="#4661E6" stroke-width="2" fill="none" fill-rule="evenodd"/></svg>
+		  )
+		}
+		
+		export default SVG;
+                
---
+++ src/icons/icon-plus.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const SVG = () => {
+  return (
+    <svg width="9" height="9" xmlns="http://www.w3.org/2000/svg">
+      <text
+        transform="translate(-24 -20)"
+        fill-rule="evenodd"
+        font-family="Jost-Bold, Jost"
+        font-size="14"
+        font-weight="bold"
+      >
+        <tspan x="24" y="27.5">
+          +
+        </tspan>
+      </text>
+    </svg>
+  );
+};
+
+export default SVG;
